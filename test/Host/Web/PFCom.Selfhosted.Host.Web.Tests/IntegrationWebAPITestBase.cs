8c27831d175b9689e8023e3e1a33231abac39479
ArtifyCZ <57506608+ArtifyCZ@users.noreply.github.com>
2021-08-20 19:14:19 +0200

    Initialized projects.

---
+++ test/Host/Web/PFCom.Selfhosted.Host.Web.Tests/IntegrationWebAPITestBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using Xunit;
+
+namespace PFCom.Selfhosted.Host.Web.Tests
+{
+    public class IntegrationWebAPITestBase : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private WebApplicationFactory<Startup> _factory { get; }
+
+        protected HttpClient Client { get; }
+
+        public IntegrationWebAPITestBase(WebApplicationFactory<Startup> factory)
+        {
+            this._factory = factory;
+            this.Client = this._factory.CreateClient();
+        }
+    }
+}
